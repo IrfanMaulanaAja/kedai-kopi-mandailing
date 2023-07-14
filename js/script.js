435390009e203b40d9f10c8ebd3544d7ab3b839f
AloAuto <irfan.maulana@alodokter.com>
2023-07-14 15:56:24 +0700

    membuat website kopi mandailing

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik di luarnside bar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
